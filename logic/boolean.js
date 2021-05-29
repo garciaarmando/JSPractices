676dc3ee12aa86baa8cc957d42eba83040dfe0ff
Armando García <armandobfmv@gmail.com>
2021-05-28 22:11:00 -0500

    Understanding booleans values

---
+++ logic/boolean.js
@@ -0,0 +1,10 @@
+let verdadero = true;
+let falso = false;
+
+console.log(verdadero, falso);
+console.log(typeof(verdadero, falso));
+
+console.log(Boolean(0)); //false
+console.log(Boolean(-1)); //true
+console.log(Boolean("")); //false
+console.log(Boolean(" ")); //true
