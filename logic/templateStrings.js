9e1d5008a234661ea438a2da77338aa8e0a937d2
Armando García <armandobfmv@gmail.com>
2021-05-28 20:02:44 -0500

    Using template strings

---
+++ logic/templateStrings.js
@@ -0,0 +1,16 @@
+let nombre = "Armando";
+let apellido = "Garcia";
+
+// let saludo = "Hola mi nombre es" + " " + nombre + " " + apellido;
+
+let saludo2 = `Hola, mi nombre es ${nombre} ${apellido}.`;
+console.log(saludo2);
+
+let ul = ` <ul>
+            <li>Primavera</li>
+            <li>Verano</li>
+            <li>Otoño</li>
+            <li>Invierno</li>
+        </ul>`;
+
+console.log(ul);
