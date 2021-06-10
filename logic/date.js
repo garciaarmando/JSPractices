ed1c1118c35740a053e63d4a2ed8e55d5837fa00
Armando García <armandobfmv@gmail.com>
2021-06-09 22:31:06 -0500

    Using differents methods for the Object Date

---
+++ logic/date.js
@@ -0,0 +1,26 @@
+//instanciamos el objeto Date() en una variable
+let fecha = new Date();
+console.log(fecha);
+//Obtener día del mes
+console.log(fecha.getDate());
+//día de la semana D L M MI J V S -> 0 1 2 3 4 5 6
+console.log(fecha.getDay());
+//obtener el mes Ene Feb Mar Abr May Jun Jul Ago Sep Oct Nov Dic -> 0 1 2 3 4 5 6 7 8 9 10 11
+console.log(fecha.getMonth());
+//para obtener el año
+console.log(fecha.getFullYear());
+//para obtener la hora, minutos,segundos y milisegundos
+console.log(fecha.getHours());
+console.log(fecha.getMinutes());
+console.log(fecha.getSeconds());
+console.log(fecha.getMilliseconds());
+//para obtener fecha a formato legible
+console.log(fecha.toString());
+//extrayendo sólo la parte de la fecha
+console.log(fecha.toDateString());
+//obtener sólo la hora local sin el huso horario
+console.log(fecha.toLocaleString());
+//obtener solo la parte de la fecha sin el huso horario
+console.log(fecha.toLocaleDateString());
+//para sólo obtener la parte de la hora sin el huso horario
+console.log(fecha.toLocaleTimeString());
