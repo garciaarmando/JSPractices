f940249a1776268108ef74a3f30b26a6269bf4b3
Armando García <armandobfmv@gmail.com>
2021-06-11 18:08:50 -0500

    On this exercise, we use regular expresion and the replace method to delete a pattern in a sting

--- logicExercises/2-4.js
+++ logicExercises/2-4.js
@@ -1,1 +1,12 @@
 // 8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
+
+const eliminarCaracteres = (texto = "", patron = "") => {
+    !texto
+        ?
+        console.warn("No se ingresó ningún texto") :
+        !patron ?
+        console.warn("No se ingresó el patrón de caracteres") :
+        console.info(texto.replace(new RegExp(patron, "ig"), ""));
+};
+
+eliminarCaracteres("lala1,lala2,lala3,lala4,lala5", "lala");
