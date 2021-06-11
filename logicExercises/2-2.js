8c64cf269657f6c7f9c4cd46998798be9ccc1c84
Armando García <armandobfmv@gmail.com>
2021-06-11 17:28:59 -0500

    Getting help to the exercise 2-2 :c

--- logicExercises/2-2.js
+++ logicExercises/2-2.js
@@ -1,1 +1,24 @@
 //6) Programa una función para contar el número de veces que se repite una palabra en un texto largo, pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2.
+
+const textoEnCadena = (cadena = "", texto = "") => {
+    if (!cadena) return console.warn("No ingresaste la cadena");
+    if (!texto) return console.warn("No ingresaste el texto");
+    let i = 0,
+        contador = 0;
+
+    while (i !== -1) {
+        i = cadena.indexOf(texto, i);
+        if (i !== -1) {
+            i++;
+            contador++;
+        }
+    }
+    return console.info(
+        `La palabra texto se repite ${contador} veces y lo que trae i es ${i}`
+    );
+};
+
+textoEnCadena(
+    "Hola mundo, adios mundo, yolo mundo, holi mundo chao mundo",
+    "mundo"
+);
--- logicExercises/2-3.js
+++ logicExercises/2-3.js
@@ -1,1 +1,21 @@
-//7) Programa una función que valide si una palabra o frase dada, es un palíndromo(que se lee igual en un sentido que en otro), pe.mifuncion("Salas") devolverá true.
+//7) Programa una función que valide si una palabra o frase dada, es un palíndromo(que se lee igual en un sentido que en otro), pe.mifuncion("Salas") devolverá true.function texto()
+
+function texto() {
+    let palabra = prompt("Escribe una palabra").toLowerCase();
+
+    // eliminamos los espacios en blanco
+    palabra = palabra.replace(/ /g, "");
+
+    for (var i = 0; i < palabra.length; i++) {
+        if (palabra[i] != palabra[palabra.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+if (texto()) {
+    alert("Esto es palíndromo");
+} else {
+    alert("Esto no es palíndromo");
+}
