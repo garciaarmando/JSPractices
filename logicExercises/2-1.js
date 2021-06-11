196c86085ca9029b17825db0e00ee2d79f36d1e1
Armando García <armandobfmv@gmail.com>
2021-06-11 14:12:32 -0500

    Finishing with the exercise 2-1

--- logicExercises/2-1.js
+++ logicExercises/2-1.js
@@ -1,1 +1,14 @@
 // 5) Programa una función que invierta las palabras de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá "odnuM aloH".
+
+let texto = prompt("Ingrese el texto a mostrar al revés");
+
+const textoInvertido = (texto = "") => {
+    if (!texto)
+        return console.warn("No se ingresó ningun valor para notar el cambio");
+    if (texto.length < 2)
+        return console.error("Por favor ingrese más de un valor");
+    if (Math.sign(texto) === -1)
+        return console.error("El valor no puede ser negativo");
+    return console.info(texto.split("").reverse().join(""));
+};
+textoInvertido(texto);
