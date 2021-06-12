d65777f729d76b30e1adf764f91a6572ce0397f5
Armando García <armandobfmv@gmail.com>
2021-06-11 22:11:17 -0500

    Again needed some help to create a function that give me the answer aboput if a number is even or odd

--- logicExercises/4-2.js
+++ logicExercises/4-2.js
@@ -1,18 +1,11 @@
 // 13) Programa una función que determine si un número es par o impar, pe. miFuncion(29) devolverá Impar.
-
-const parImpar = (numero = 0) => {
-    if (!numero) return console.error("No se colocó un número");
+const parImpar = (numero = undefined) => {
+    if (numero === undefined) return console.warn("No ingresaste un número");
     if (typeof numero !== "number")
-        return console.error("El valor ingresado no es un número");
-    if (numero === 0) console.error("El número no puede ser 0");
-    if (Math.sign(numero) === -1)
-        return console.error("El número no puede ser negativo");
-    numero = numero.toString();
-    if (numero.charAt(numero.length - 1) === "0" || "2" || "4" || "6" || "8") {
-        console.warn(`El número ${numero} es un número primo`);
-    } else {
-        console.warn(`El número ${numero} no es un número primo`);
-    }
+        return console.error(`El valor ${numero} ingresado NO es un número`);
+    return numero % 2 === 0 ?
+        console.info(`El número ${numero} es par`) :
+        console.info(`El número ${numero} es impar`);
 };
 
-parImpar(0);
+parImpar(-398);
--- logicExercises/4-3.js
+++ logicExercises/4-3.js
@@ -1,3 +1,1 @@
 // 14) Programa una función para convertir grados Celsius a Fahrenheit y viceversa, pe. miFuncion(0,"C") devolverá 32°F.
-
-let numero = prompt("Ingresa un número");
