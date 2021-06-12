9dbbc1a14d1b605d098754751d90a66113f48e09
Armando García <armandobfmv@gmail.com>
2021-06-12 12:06:26 -0500

    Some comments were removed

--- logicExercises/4-3.js
+++ logicExercises/4-3.js
@@ -1,8 +1,3 @@
-// 14) Programa una función para convertir grados Celsius a Fahrenheit y viceversa, pe. miFuncion(0,"C") devolverá 32°F.
-
-//1 celsiu a cuántos farenheit equivale?
-//numero, detonaror (f,c) que cambiará el valor por el que se multiplicará el número
-
 const convertirGrados = (grados = undefined, unidad = undefined) => {
     if (grados === undefined)
         return console.warn("No ingresaste grados a convertir");
