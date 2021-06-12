977365421d7bf7a52a390efa62cd0261afffc48b
Armando García <armandobfmv@gmail.com>
2021-06-11 20:20:48 -0500

    Getting some help to the exercise 3-3

--- logicExercises/3-3.js
+++ logicExercises/3-3.js
@@ -1,1 +1,15 @@
-// 11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
+const factorial = (numero = undefined) => {
+    if (numero === undefined) return console.warn("No se colocó un número");
+    if (typeof numero !== "number")
+        return console.error("El valor ingresado no es un número");
+    if (numero === 0) console.error("El número no puede ser 0");
+    if (Math.sign(numero) === -1)
+        return console.error("El número no puede ser negativo");
+    let factorial = 1;
+    for (let i = numero; i > 1; i--) {
+        factorial *= i;
+    }
+    return console.info(`El factorial de ${numero} es ${factorial}`);
+};
+
+factorial(8);
