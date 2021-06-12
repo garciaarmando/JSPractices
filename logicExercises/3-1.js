9f460d2032ff86f3de1ec7177b426a295300da03
Armando García <armandobfmv@gmail.com>
2021-06-11 19:02:19 -0500

    create a function that receives a number and say if the number is a capicua

--- logicExercises/3-1.js
+++ logicExercises/3-1.js
@@ -1,6 +1,5 @@
 // 9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
 const numeroAleatorio = (min = 501, max = 600) => {
-    // (min = 501), (max = 600);
     return Math.round(Math.random() * (max - min) + min);
 };
 
--- logicExercises/3-2.js
+++ logicExercises/3-2.js
@@ -1,1 +1,18 @@
 // 10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
+
+let texto = prompt("Ingresa un número para validar si se trata de capicúa");
+
+const numeroCapicua = (texto = "") => {
+    if (!texto) return console.warn("No ingresaste una palabra o frase");
+
+    let alreves = texto.split("").reverse().join("");
+    return texto === alreves ?
+        console.info(
+            `El número ${texto} se lee igual que el número ${alreves}, es un Capicúa!`
+        ) :
+        console.info(
+            `El número ${texto} no se lee igual que el número ${alreves}, no es un Capicúa :(`
+        );
+};
+
+numeroCapicua(texto);
