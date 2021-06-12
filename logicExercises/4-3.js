8646cdd1943a621aabd65d84fe97384bda99ce7b
Armando García <armandobfmv@gmail.com>
2021-06-11 22:21:55 -0500

    Starting with a functions that convert celsius to farenheit and vice versa

--- logicExercises/4-3.js
+++ logicExercises/4-3.js
@@ -1,1 +1,4 @@
 // 14) Programa una función para convertir grados Celsius a Fahrenheit y viceversa, pe. miFuncion(0,"C") devolverá 32°F.
+
+//1 celsiu a cuántos farenheit equivale?
+//numero, detonaror (f,c) que cambiará el valor por el que se multiplicará el número
