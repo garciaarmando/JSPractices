46ed5772d091c3ca14d81b2a5e21cdf5e6bc4b96
Armando García <armandobfmv@gmail.com>
2021-06-15 14:28:21 -0500

    Some fixes

--- logicExercises/9-1.js
+++ logicExercises/9-1.js
@@ -1,23 +1,24 @@
 /*
-27) Programa una clase llamada Pelicula.
+27)
+
+Programa una clase llamada Pelicula usando sólo vanila JS.
 
 La clase recibirá un objeto al momento de instanciarse con los siguentes datos: id de la película en IMDB, titulo, director, año de estreno, país o países de origen, géneros y calificación en IMBD.
-  (IN PROGRESS)Todos los datos del objeto son obligatorios.
-  (DONE)Valida que el id IMDB tenga 9 caracteres, los primeros 2 sean letras y los 7 restantes números.
-  (DONE)Valida que el título no rebase los 100 caracteres.
-  (DONE)Valida que el director no rebase los 50 caracteres.
-  (DONE)Valida que el año de estreno sea un número entero de 4 dígitos.
-  (DONE)Valida que el país o paises sea introducidos en forma de arreglo.
-  - Valida que los géneros sean introducidos en forma de arreglo.
-  - Valida que los géneros introducidos esten dentro de los géneros aceptados*.
-  (DONE)Crea un método estático que devuelva los géneros aceptados*.
-  - Valida que la calificación sea un número entre 0 y 10 pudiendo ser decimal de una posición.
-  - Crea un método que devuelva toda la ficha técnica de la película.
-  - Apartir de un arreglo con la información de 3 películas genera 3 instancias de la clase de forma automatizada e imprime la ficha técnica
-    de cada película.
+(IN PROGRESS)Todos los datos del objeto son obligatorios.
+(DONE)Valida que el id IMDB tenga 9 caracteres, los primeros 2 sean letras y los 7 restantes números.
+(DONE)Valida que el título no rebase los 100 caracteres.
+(DONE)Valida que el director no rebase los 50 caracteres.
+(DONE)Valida que el año de estreno sea un número entero de 4 dígitos.
+(DONE)Valida que el país o paises sea introducidos en forma de arreglo.
+- Valida que los géneros sean introducidos en forma de arreglo.
+- Valida que los géneros introducidos esten dentro de los géneros aceptados*.
+(DONE)Crea un método estático que devuelva los géneros aceptados*.
+- Valida que la calificación sea un número entre 0 y 10 pudiendo ser decimal de una posición.
+- Crea un método que devuelva toda la ficha técnica de la película.
+- Apartir de un arreglo con la información de 3 películas genera 3 instancias de la clase de forma automatizada e imprime la ficha técnica de cada película.
 
 * Géneros Aceptados: Action, Adult, Adventure, Animation, Biography, Comedy, Crime, Documentary ,Drama, Family, Fantasy, Film Noir, Game-Show, History, Horror, Musical, Music, Mystery, News, Reality-TV, Romance, Sci-Fi, Short, Sport, Talk-Show, Thriller, War, Western.
- */
+*/
 
 class Pelicula {
     constructor({ id, titulo, director, estreno, pais, generos, calificacion }) {
