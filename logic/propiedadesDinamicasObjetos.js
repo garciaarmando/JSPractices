d2c2f6c700c31c3850e35d21daa4a4df8412dbf9
Armando García <armandobfmv@gmail.com>
2021-06-21 12:14:40 -0500

    Using dynamic properties in an object

---
+++ logic/propiedadesDinamicasObjetos.js
@@ -0,0 +1,11 @@
+let aleatorio = Math.round(Math.random() * 100 + 5);
+const objUsuarios = {
+    // [`id_${aleatorio}`]: "Valor aleatorio",
+};
+
+console.log(objUsuarios);
+const usuarios = ["Armando", "Jon", "María", "Dexter", "Jenny"];
+
+usuarios.forEach((usuario, index) => (objUsuarios[`id_${index}`] = usuario));
+
+console.log(objUsuarios);
