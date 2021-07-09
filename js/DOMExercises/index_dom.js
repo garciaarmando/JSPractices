ac727e69041f5a2a33c053845f92ff7df9cc2ace
Armando García <armandobfmv@gmail.com>
2021-07-09 13:58:01 -0500

    Creating a dark mode

--- js/DOMExercises/index_dom.js
+++ js/DOMExercises/index_dom.js
@@ -1,6 +1,6 @@
 import scrollTopButton from "./DOMfiles/button_scroll.js";
 import countdown from "./DOMfiles/countdown.js";
-import darkMode from "./DOMfiles/dark-mode.js";
+import darkTheme from "./DOMfiles/dark-mode.js";
 import { moveBall, shortCuts } from "./DOMfiles/eventos-teclado.js";
 import hamburgerMenu from "./DOMfiles/menu_hamburguesa.js";
 import { alarm, digitalClock } from "./DOMfiles/reloj.js";
@@ -16,7 +16,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
     );
     countdown("countdown", "Jan 04, 2022 00:00:00", "Felices 27 🤓");
     scrollTopButton(".scroll-top-btn");
-    darkMode();
+    darkTheme(".dark-theme-btn", "dark-mode");
 });
 
 d.addEventListener("keydown", (e) => {
