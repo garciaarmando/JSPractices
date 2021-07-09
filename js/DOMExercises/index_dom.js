2f2ae02003c199d2f57503bd0c711f302966678f
Armando García <armandobfmv@gmail.com>
2021-07-08 19:35:42 -0500

    Making a countdown

--- js/DOMExercises/index_dom.js
+++ js/DOMExercises/index_dom.js
@@ -1,3 +1,4 @@
+import countdown from "./DOMfiles/countdown.js";
 import { moveBall, shortCuts } from "./DOMfiles/eventos-teclado.js";
 import hamburgerMenu from "./DOMfiles/menu_hamburguesa.js";
 import { alarm, digitalClock } from "./DOMfiles/reloj.js";
@@ -11,10 +12,9 @@ d.addEventListener("DOMContentLoaded", (e) => {
         "#activar-alarma",
         "#desactivar-alarma"
     );
+    countdown("countdown", "Jan 04, 2022 05:32:00", "Felices 27 🤓");
 });
-//keydown presionar tecla
-//keyup dejar de presionarla
-//keypress mientras se mantenga presionada la tecla
+
 d.addEventListener("keydown", (e) => {
     shortCuts(e);
     moveBall(e, ".ball", ".stage");
