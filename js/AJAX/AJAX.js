dba0a34dce7005f10a1716aa8c0912ae92e7b056
Armando García <armandobfmv@gmail.com>
2021-07-12 13:18:20 -0500

    Axios with async-await

--- js/AJAX/AJAX.js
+++ js/AJAX/AJAX.js
@@ -116,8 +116,36 @@
             $axios.innerHTML = `<p>Error ${err.response.status}: ${errorMessage}</p>`;
         })
         .finally(() => {
-            console.log(
-                "Esto se ejecutará independientemente del resultado de Axios"
-            );
+            /* console.log(
+                                                                                                    "Esto se ejecutará independientemente del resultado de Axios"
+                                                                                                ); */
         });
+})();
+/**********AXIOS + Async-Await**********/
+(() => {
+    const $axiosAsync = document.getElementById("axios-async"),
+        $fragment = document.createDocumentFragment();
+
+    async function getData() {
+        try {
+            let res = await axios.get("https://jsonplaceholder.typicode.com/users"),
+                json = await res.data;
+            json.forEach((el) => {
+                const $li = document.createElement("li");
+                $li.innerHTML = `${el.name} | ${el.email} | ${el.phone}`;
+                $fragment.appendChild($li);
+            });
+            $axiosAsync.appendChild($fragment);
+        } catch (err) {
+            let errorMessage =
+                err.response.statusText ||
+                "Ocurrió un error al tratar de obtener los datos";
+            $axiosAsync.innerHTML = `<p>Error ${err.response.status}: ${errorMessage}</p>`;
+        } finally {
+            console.log(
+                "Esto se ejecutará independientemente del resultado de axios-async"
+            );
+        }
+    }
+    getData();
 })();
