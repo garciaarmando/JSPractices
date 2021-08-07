ce704b360f082b089b45bdf925313d496e47a8b4
Armando García <armandobfmv@gmail.com>
2021-08-06 21:47:47 -0500

    Delete stripe-keys.js

--- js/ajax-excercises/stripe/stripe-keys.js
+++
@@ -1,4 +0,0 @@
-export default {
-    public: "pk_test_51JIxRfBkEB8jjgUDp9V3C6IA0o3eIapNLbLsIaMJVJfP8Bau8yWmKb8RLxAUVX0YTLYZhSDl2S4MSvTVaki0cS8e00qAe3OzkX",
-    secret: "sk_test_51JIxRfBkEB8jjgUDAQKeVdlstu7PTW17obKWskLaFLvTdPZ6vETaZ8MN99YRThWsPvAkXUruoLl61Zev7NVkvCKi00fvMrOqUI",
-};
