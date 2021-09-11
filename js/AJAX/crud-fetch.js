220a4921a86cf0b10b8828351910206972d4859b
Armando García <armandobfmv@gmail.com>
2021-09-10 23:25:36 -0500

    CRUD with fetch and axios library

--- js/AJAX/crud-fetch.js
+++ js/AJAX/crud-fetch.js
@@ -9,21 +9,24 @@ const getAll = async() => {
     try {
         let res = await fetch("http://localhost:5555/santos"),
             json = await res.json();
-        if (!res.ok) throw { status: res.status, statusText: res.statusText };
+
+        if (!res.ok) throw { status: res.status, message: res.statusText };
 
         json.forEach((el) => {
             $template.querySelector(".name").textContent = el.nombre;
-            $template.querySelector(".constelation").textContent = el.constelacion;
+            $template.querySelector(".constellation").textContent = el.constelacion;
             $template.querySelector(".edit").dataset.id = el.id;
             $template.querySelector(".edit").dataset.name = el.nombre;
             $template.querySelector(".edit").dataset.constellation = el.constelacion;
             $template.querySelector(".delete").dataset.id = el.id;
+
             let $clone = d.importNode($template, true);
             $fragment.appendChild($clone);
         });
+
         $table.querySelector("tbody").appendChild($fragment);
     } catch (err) {
-        let message = err.status || "Ocurrió un error";
+        let message = err.statusText || "Ocurrió un error";
         $table.insertAdjacentHTML(
             "afterend",
             `<p><b>Error ${err.status}: ${message}</b></p>`
@@ -32,55 +35,57 @@ const getAll = async() => {
 };
 
 d.addEventListener("DOMContentLoaded", getAll);
+
 d.addEventListener("submit", async(e) => {
     if (e.target === $form) {
         e.preventDefault();
         if (!e.target.id.value) {
-            //Create POST
+            //CREATE - PIOST
             try {
                 let options = {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json; charset=utf-8",
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json; charset=utf-8",
+                        },
+                        body: JSON.stringify({
+                            nombre: e.target.nombre.value,
+                            constelacion: e.target.constelacion.value,
+                        }),
                     },
-                    body: JSON.stringify({
-                        nombre: e.target.nombre.value,
-                        constelacion: e.target.constelacion.value,
-                    }),
-                };
-                let res = await fetch("http://localhost:5555/santos", options);
-                json = await res.json();
-                if (!res.ok) throw { status: res.status, statusText: res.statusText };
+                    res = await fetch("http://localhost:5555/santos", options),
+                    json = await res.json();
+                if (!res.ok) throw { status: res.status, message: res.statusText };
                 location.reload();
-            } catch (error) {
-                let message = err.status || "Ocurrió un error";
+            } catch (err) {
+                let message = err.statusText || "Ocurrió un error";
                 $form.insertAdjacentHTML(
                     "afterend",
                     `<p><b>Error ${err.status}: ${message}</b></p>`
                 );
             }
         } else {
-            //Update - PUT
+            //UPDATE - PUT
+
             try {
                 let options = {
-                    method: "PUT",
-                    headers: {
-                        "Content-Type": "application/json; charset=utf-8",
+                        method: "PUT",
+                        headers: {
+                            "Content-Type": "application/json; charset=utf-8",
+                        },
+                        body: JSON.stringify({
+                            nombre: e.target.nombre.value,
+                            constelacion: e.target.constelacion.value,
+                        }),
                     },
-                    body: JSON.stringify({
-                        nombre: e.target.nombre.value,
-                        constelacion: e.target.constelacion.value,
-                    }),
-                };
-                let res = await fetch(
-                    `http://localhost:555/santos/${e.target.id.value}`,
-                    options
-                );
-                json = await res.json();
-                if (!res.ok) throw { status: res.status, statusText: res.statusText };
+                    res = await fetch(
+                        `http://localhost:5555/santos/${e.target.id.value}`,
+                        options
+                    ),
+                    json = await res.json();
+                if (!res.ok) throw { status: res.status, message: res.statusText };
                 location.reload();
-            } catch (error) {
-                let message = err.status || "Ocurrió un error";
+            } catch (err) {
+                let message = err.statusText || "Ocurrió un error";
                 $form.insertAdjacentHTML(
                     "afterend",
                     `<p><b>Error ${err.status}: ${message}</b></p>`
@@ -89,11 +94,38 @@ d.addEventListener("submit", async(e) => {
         }
     }
 });
-d.addEventListener("click", (e) => {
+d.addEventListener("click", async(e) => {
     if (e.target.matches(".edit")) {
         $title.textContent = "Editar Santo";
         $form.nombre.value = e.target.dataset.name;
         $form.constelacion.value = e.target.dataset.constellation;
         $form.id.value = e.target.dataset.id;
+    }
+    if (e.target.matches(".delete")) {
+        let isDelete = confirm(
+            `¿Estás seguro de eliminar el id ${e.target.dataset.id}?`
+        );
+        if (isDelete) {
+            try {
+                let options = {
+                        method: "DELETE",
+                        headers: {
+                            "Content-Type": "application/json; charset=utf-8",
+                        },
+                    },
+                    res = await fetch(
+                        `http://localhost:5555/santos/${e.target.dataset.id}`,
+                        options
+                    ),
+                    json = await res.json();
+
+                if (!res.ok) throw { status: res.status, message: res.statusText };
+
+                location.reload();
+            } catch (err) {
+                let message = err.statusText || "Ocurrió un error";
+                alert(`Error ${err.status}: ${message}`);
+            }
+        }
     }
 });
