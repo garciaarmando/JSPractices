6c9a763abb3e9e9c13a70b12e380af8533c2810e
Armando García <armandobfmv@gmail.com>
2021-09-21 21:20:32 -0500

    The pokemons pagination it's done 2/2

--- js/ajax-excercises/api-pokemon.js
+++ js/ajax-excercises/api-pokemon.js
@@ -13,7 +13,7 @@ async function loadPokemons(url) {
             $prevLink,
             $nextLink;
 
-        // console.log(json);
+        console.log(json);
 
         if (!res.ok) throw { status: res.status, statusText: res.statusText };
 
@@ -33,19 +33,28 @@ async function loadPokemons(url) {
               </figure>
               `;
             } catch (err) {
-                console.log(err);
+                // console.log(err);
                 let message = err.statusText || "Ocurrió un error";
                 $template += `<figure>
-              <figcaption>${err.status}: ${message}</figcaption>  
+              <figcaption>${err.status}: ${message}</figcaption>
               </figure> `;
             }
         }
         $main.innerHTML = $template;
+        $prevLink = json.previous ? `<a href="${json.previous}">⏮️</a>` : "";
+        $nextLink = json.next ? `<a href="${json.next}">⏭️</a>` : "";
+        $links.innerHTML = `${$prevLink} ${$nextLink}`;
     } catch (err) {
-        console.log(err);
+        // console.log(err);
         let message = err.statusText || "Ocurrió un error";
         $main.innerHTML = `<p>Error ${err.status}: ${message}</p>`;
     }
 }
 
 d.addEventListener("DOMContentLoaded", (e) => loadPokemons(pokeAPI));
+d.addEventListener("click", (e) => {
+    if (e.target.matches(".links a")) {
+        e.preventDefault();
+        loadPokemons(e.target.getAttribute("href"));
+    }
+});
